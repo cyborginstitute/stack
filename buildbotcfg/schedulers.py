e278267b6148c1ece569a9c05f526106954584d4
tycho garen <garen@tychoish.com>
2012-06-12 22:19:42 -0400

    buildbot config updating and incrementing to add polled rebuilds

--- buildbotcfg/schedulers.py
+++ buildbotcfg/schedulers.py
@@ -3,24 +3,30 @@ from buildbot.schedulers.forcesched import ForceScheduler, FixedParameter, Strin
 from buildbot.schedulers.timed import Periodic
 from buildbot.schedulers.trysched import Try_Userpass
 
-# from institutebb.slaves import slaves
-from institutebb import builders
-from institutebb import changes
+from buildbot.changes.filter import ChangeFilter
+
+builder_names = [ "institute-site", "institute-internal" ]
+polled_builder_names = [ "polled-builder" ]
+
+all_builder_names = builder_names + polled_builder_names
 
 schedulers = []
-schedulers.append(SingleBranchScheduler(name="all", 
-                                        branch='master',
-                                        treeStableTimer=None,
-                                        builderNames=builders.builder_names))
 
+manual_builder = ChangeFilter(project=builder_names, branch='master')
+schedulers.append(SingleBranchScheduler(name="all", 
+                                        treeStableTimer=None,
+                                        change_filter=manual_builder,
+                                        builderNames=builder_names))
+
+polled_builder = ChangeFilter(project='polled-docs', branch='master')
 schedulers.append(SingleBranchScheduler(name="mongodb", 
                                         treeStableTimer=None,
-                                        change_filter=changes.mongodb_filter,
-                                        builderNames=builders.polled_builder_names))
+                                        change_filter=polled_filter,
+                                        builderNames=polled_builder_names))
 
 schedulers.append(ForceScheduler(name="force",
                                  repository=FixedParameter(name="repository"),
                                  branch=StringParameter(name="branch", default="master"),
                                  project=FixedParameter(name="project", default=""),
                                  properties=[],
-                                 builderNames=builders.builder_names))
+                                 builderNames=all_builder_names))
--- buildbotcfg/status.py
+++ buildbotcfg/status.py
@@ -3,16 +3,21 @@ status = []
 from buildbot.status import html
 from buildbot.status.web.authz import Authz
 from buildbot.status.web.auth import BasicAuth
+from buildbot import revlinks
 
 users = [ ('dev', 'bbot!')] # it's not *that* secret..
 authz = Authz(auth=BasicAuth(users),
 	forceBuild='auth',
 )
+
+
+revlink = revlinks.RevlinkMatch([r'git://github.com/(.*)/(.*)'], r'https://github.com/\1/\2/commit/%s')
+
+
 status.append(html.WebStatus(
 		http_port=8010,
 		authz=authz,
 		order_console_by_time=True,
-		revlink="http://github.com/cyborginstitute/institute/commit/%s",
 		change_hook_dialects={ 'github' : True },
 		))
 
